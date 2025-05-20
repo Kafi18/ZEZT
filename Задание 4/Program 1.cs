26357096836d9f3a0f4d1a8e74a22b4324d098cf
Kafi18 <j.koltsov@mail.ru>
2025-05-20 04:08:05 +0300

    Добавил новое задание

---
+++ Задание 4/Program 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class DepositCalculator
+{
+    static void Main()
+    {
+        decimal sum = ReadDecimal("Введите сумму вклада: ");
+        int months = ReadInt("Введите количество месяцев: ");
+
+        for (int i = 0; i < months; i++)
+            sum *= 1.07m;
+
+        Console.WriteLine($"Конечная сумма вклада: {sum:F2}");
+    }
+
+    static decimal ReadDecimal(string prompt)
+    {
+        Console.Write(prompt);
+        return decimal.Parse(Console.ReadLine());
+    }
+
+    static int ReadInt(string prompt)
+    {
+        Console.Write(prompt);
+        return int.Parse(Console.ReadLine());
+    }
+}
---
+++ Задание 4/Program 3.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class P
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; Console.WriteLine(), i++)
+            for (int j = 1; j <= 10; Console.Write($"{i * j}\t"), j++) ;
+    }
+}
